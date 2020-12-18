275772709f93624950a07170c861e7291e901cf0
Ameer Kiani <ameerkiani@icloud.com>
2020-12-17 17:38:16 -0800

    gitignore not working

--- final/credentials/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-// credentials for website
-
-define('DB_NAME', 'ameerc5_homework');
-define('DB_USER', 'ameerc5_ameer');
-define('DB_PASSWORD', 'pYnZtdjmQG9nv6d');
-define('DB_HOST', 'localhost');
