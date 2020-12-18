7fbec2e9138c9fbb4d21e3d6d6bacead72a8bcdb
Ameer Kiani <ameerkiani@icloud.com>
2020-12-17 17:33:13 -0800

    gitignore

---
+++ final/credentials/credentials.php
@@ -0,0 +1,7 @@
+<?php
+// credentials for website
+
+define('DB_NAME', 'ameerc5_homework');
+define('DB_USER', 'ameerc5_ameer');
+define('DB_PASSWORD', 'pYnZtdjmQG9nv6d');
+define('DB_HOST', 'localhost');
